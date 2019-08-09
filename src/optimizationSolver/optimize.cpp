b13677889653a29d2b54bbf65d45f54fb7deb822
Kristine Hagen <kristine@spacemaker.ai>
2019-08-09 14:20:34 +0200

    Split optimize function into smaller functions

--- src/optimizationSolver/optimize.cpp
+++ src/optimizationSolver/optimize.cpp
@@ -7,60 +7,61 @@
 float MAX_HEIGHT = 20.0;
 float MIN_HEIGHT = 5.0;
 float HEIGHT_INCREMENT = 1.0;
-float MAX_AVERAGE_HEIGHT = 10;
+float MAX_AVERAGE_HEIGHT = 18;
 
-SolutionCandidates createSolutionCandidatesByChangingOneBuildingHeight(int buildingIndexToChange, Buildings buildings, ObjectiveToggles objectiveToggles);
-void addFeasibleSolutionCandidatesToSolutionCandidateList(SolutionCandidates solutionCandidates, SolutionCandidates potentialSolutionCandidates);
+SolutionCandidates increaseAndDecreaseHeightOfBuilding(int buildingIndexToChange, Buildings buildings, ObjectiveToggles objectiveToggles);
+void addFeasibleSolutionCandidatesToSolutionCandidateList(SolutionCandidates *solutionCandidates, SolutionCandidates potentialSolutionCandidates);
 
 
 Buildings optimizeBuildings(Buildings initialBuildings, ObjectiveToggles objectiveToggles)
 {
     SolutionCandidates solutionCandidates;
-    solutionCandidates.push_back({createSolutionCandidateFromBuildings(initialBuildings, objectiveToggles)});
+    solutionCandidates.push_back({createSolutionCandidateFromBuildings(initialBuildings, objectiveToggles, MAX_AVERAGE_HEIGHT)});
 
     for (int buildingIndex = 0; buildingIndex < int (initialBuildings.size()); buildingIndex += 1)
     {
-        SolutionCandidates potentialSolutionCandidates = createSolutionCandidatesByChangingOneBuildingHeight(buildingIndex, initialBuildings, objectiveToggles);
-        addFeasibleSolutionCandidatesToSolutionCandidateList(solutionCandidates, potentialSolutionCandidates);
+        SolutionCandidates potentialSolutionCandidates = increaseAndDecreaseHeightOfBuilding(buildingIndex, initialBuildings, objectiveToggles);
+        addFeasibleSolutionCandidatesToSolutionCandidateList(&solutionCandidates, potentialSolutionCandidates);
     }
     SolutionCandidate bestSolutionCandidate = getBestSolutionCandidate(solutionCandidates);
     return bestSolutionCandidate.buildings;
 }
 
-SolutionCandidates createSolutionCandidatesByChangingOneBuildingHeight(int buildingIndexToChange, Buildings buildings, ObjectiveToggles objectiveToggles)
+
+Buildings changeHeightOfBuilding(Buildings buildings, int buildingIndexToChange, float heightIncrement)
+{
+    float currentHeight = buildings[buildingIndexToChange].height;
+    Buildings updatedBuildings = buildings;
+    updatedBuildings[buildingIndexToChange].height = currentHeight + heightIncrement;
+    return updatedBuildings;
+}
+
+
+SolutionCandidates increaseAndDecreaseHeightOfBuilding(int buildingIndexToChange, Buildings buildings, ObjectiveToggles objectiveToggles)
 {
     SolutionCandidates potentialSolutionCandidates;
-
     float currentHeight = buildings[buildingIndexToChange].height;
     if (currentHeight <= MAX_HEIGHT + HEIGHT_INCREMENT)
     {
-        Buildings updatedBuildings = buildings;
-        updatedBuildings[buildingIndexToChange].height = currentHeight + HEIGHT_INCREMENT;
-        if (solutionIsFeasible(updatedBuildings, MAX_AVERAGE_HEIGHT)){
-            SolutionCandidate solutionCandidateFromUpdatedBuildings = createSolutionCandidateFromBuildings(updatedBuildings, objectiveToggles);
-            potentialSolutionCandidates.push_back(solutionCandidateFromUpdatedBuildings);
-        }
+        Buildings buildingsWithIncreasedHeight = changeHeightOfBuilding(buildings, buildingIndexToChange, HEIGHT_INCREMENT);
+        potentialSolutionCandidates.push_back({createSolutionCandidateFromBuildings(buildingsWithIncreasedHeight, objectiveToggles, MAX_AVERAGE_HEIGHT)});
     }
     if (currentHeight >= MIN_HEIGHT - HEIGHT_INCREMENT)
     {
-        Buildings updatedBuildings = buildings;
-        updatedBuildings[buildingIndexToChange].height = currentHeight - HEIGHT_INCREMENT;
-        if (solutionIsFeasible(updatedBuildings, MAX_AVERAGE_HEIGHT)) {
-            SolutionCandidate solutionCandidateFromUpdatedBuildings = createSolutionCandidateFromBuildings(updatedBuildings, objectiveToggles);
-            potentialSolutionCandidates.push_back(solutionCandidateFromUpdatedBuildings);
-        }
+        Buildings buildingsWithIncreasedHeight = changeHeightOfBuilding(buildings, buildingIndexToChange, -HEIGHT_INCREMENT);
+        potentialSolutionCandidates.push_back({createSolutionCandidateFromBuildings(buildingsWithIncreasedHeight, objectiveToggles, MAX_AVERAGE_HEIGHT)});
     }
     return potentialSolutionCandidates;
 }
 
 
-void addFeasibleSolutionCandidatesToSolutionCandidateList(SolutionCandidates solutionCandidates, SolutionCandidates potentialSolutionCandidates)
+void addFeasibleSolutionCandidatesToSolutionCandidateList(SolutionCandidates *solutionCandidates, SolutionCandidates potentialSolutionCandidates)
 {
     for (SolutionCandidate potentialSolutionCandidate: potentialSolutionCandidates)
     {
-        if (potentialSolutionCandidate.solutionIsFeasible)
+        if (potentialSolutionCandidate.isFeasible)
         {
-            solutionCandidates.push_back(potentialSolutionCandidate);
+            solutionCandidates->push_back(potentialSolutionCandidate);
         }
     }
 }
