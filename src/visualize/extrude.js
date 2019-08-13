0633a825187100794a874bf4d8ffbb9e32f093cb
Håvard Wormdal Høiby <havardwhoiby@gmail.com>
2019-08-13 22:20:50 +0200

    step 3

--- src/visualize/extrude.js
+++ src/visualize/extrude.js
@@ -16,6 +16,11 @@ const createBuilding = (coordinates, height) => {
   // Think of this as if you where drawing on paper.
   // `moveTo` moves your pencil without touching the paper
   // `lineTo` will a staight line from your current position to the new point
+  shape.moveTo(x0, y0);
+  shape.lineTo(x1, y1);
+  shape.lineTo(x2, y2);
+  shape.lineTo(x3, y3);
+  shape.lineTo(x0, y0);
 
   const geometry = new THREE.ExtrudeGeometry(shape, {
     depth: height,
