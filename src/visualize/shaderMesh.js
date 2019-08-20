2e6d10c08101d3f906e414de1be358fce437b591
paal_rasmussen <paal@spacemaker.ai>
2019-08-20 16:29:23 +0200

    greeeen

--- src/visualize/shaderMesh.js
+++ src/visualize/shaderMesh.js
@@ -6,7 +6,8 @@ const vertexShader = `
     varying float roof;
     
     void main() {
-      distanceVec = position - distance_position; 
+      distanceVec = position - distance_position;
+      // distanceVec = vec3(0.0, 0.0, 0.0);
       vec4 modelViewPosition = modelViewMatrix * vec4(position, 1.0);
       roof = abs(normal.z) > 0.001 ? 1.0 : 0.0 ;
       gl_Position = projectionMatrix * modelViewPosition; 
@@ -21,14 +22,15 @@ const fragmentShader = `
       void main() {
         float distance_squared = distanceVec.x * distanceVec.x + distanceVec.y * distanceVec.y + distanceVec.z * distanceVec.z;
         float distance = pow(distance_squared, 0.5);
+        // float distance = 0.0;
         if (roof > 0.0) {
             gl_FragColor = vec4(1.0, 1.0,1.0, 1.0);
         } else {   
             float scaled_distance = distance / 100.;
             gl_FragColor = vec4(
-                scaled_distance,
-                scaled_distance,
-                scaled_distance,
+                0.1,
+                1.0 - scaled_distance,
+                0.1,
                 1.0
             );
         }
