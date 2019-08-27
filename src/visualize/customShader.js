b955cec5e55718564d3065e8f8312fd8512998f3
guroseter <guro@spacemaker.ai>
2019-08-27 13:03:55 +0200

    Updated with variable names from master

--- src/visualize/customShader.js
+++ src/visualize/customShader.js
@@ -1,22 +1,22 @@
 import * as THREE from "../three.js";
 
 const vertexShader = `
-    varying vec3 distanceVec; // pass the distance vector to the fragment shader
+    varying vec3 vectorBetweenBusStopAndVertex; // pass the distance vector to the fragment shader
     uniform vec3 busStopPosition; // uniform passed from the material
     
     void main() {
-      distanceVec = position-busStopPosition;
+      vectorBetweenBusStopAndVertex = position-busStopPosition;
       vec4 modelViewPosition = modelViewMatrix * vec4(position, 1.0);
       gl_Position = projectionMatrix * modelViewPosition; 
     }
   `;
 
 const fragmentShader = `
-      varying vec3 distanceVec; // interpolated from the values set for each vertex in the vertex shader
+      varying vec3 vectorBetweenBusStopAndVertex; // interpolated from the values set for each vertex in the vertex shader
 
       void main() {
-        float distance = sqrt(dot(distanceVec, distanceVec));
-        float scaledDistance = distance / 400.;
+        float distanceBetweenBusStopAndFragment = sqrt(dot(vectorBetweenBusStopAndVertex, vectorBetweenBusStopAndVertex));
+        float scaledDistance = distanceBetweenBusStopAndFragment / 300.;
         gl_FragColor = vec4(
             0.1,
             1.0 - scaledDistance, // set the green channel of the pixel color to a shading of green depending on the computed distance
